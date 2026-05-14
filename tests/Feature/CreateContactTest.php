e3df18a0d518d85aa004df8e90c8bfda34b34dee
Styl Bandeira <stylbandeira@gmail.com>
2026-05-14 09:15:10 -0300

    test: create errors for contact creation

--- tests/Feature/CreateContactTest.php
+++ tests/Feature/CreateContactTest.php
@@ -9,14 +9,14 @@ use Tests\TestCase;
 class CreateContactTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Assert that a contact can be created.
      */
-    public function test_example(): void
+    public function create_contact_is_ok(): void
     {
         $response = $this->postJson('/api/contacts', [
             'name' => 'Maria de Lourdes',
             'email' => 'maria@email.com',
-            'phone' => '87996236447'
+            'phone' => '87996236447',
         ]);
 
         $response->assertCreated();
@@ -24,5 +24,64 @@ class CreateContactTest extends TestCase
         $this->assertDatabaseHas('contacts', [
             'email' => 'maria@email.com',
         ]);
+    }
+
+    /**
+     * Assert that a created contact has its default fields.
+     */
+    public function created_contact_has_default_fields(): void
+    {
+        $response = $this->postJson('/api/contacts', [
+            'name' => 'Maria de Lourdes',
+            'email' => 'maria@email.com',
+            'phone' => '87996236447',
+        ]);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('contacts', [
+            'email' => 'maria@email.com',
+            'status' => 'pending',
+            'score' => '0'
+        ]);
+    }
+
+    /**
+     * Assert that a created contact with no name fails.
+     */
+    public function created_contact_with_no_name_dont_pass(): void
+    {
+        $response = $this->postJson('/api/contacts', [
+            'email' => 'maria@email.com',
+            'phone' => '87996236447',
+        ]);
+
+        $response->assertJsonValidationErrorFor('name');
+    }
+
+    /**
+     * Assert that a created contact with no email fails.
+     */
+    public function created_contact_with_no_email_dont_pass(): void
+    {
+        $response = $this->postJson('/api/contacts', [
+            'name' => 'Maria de Lourdes',
+            'phone' => '87996236447',
+        ]);
+
+        $response->assertJsonValidationErrorFor('email');
+    }
+
+    /**
+     * Assert that a created contact with no phone fails.
+     */
+    public function created_contact_with_no_phone_dont_pass(): void
+    {
+        $response = $this->postJson('/api/contacts', [
+            'name' => 'Maria de Lourdes',
+            'email' => 'maria@email.com',
+        ]);
+
+        $response->assertJsonValidationErrorFor('phone');
     }
 }

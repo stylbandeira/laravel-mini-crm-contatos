f6c9ab2ffa30154a4b2472d2683ed2527977cd1c
Styl Bandeira <stylbandeira@gmail.com>
2026-05-14 11:32:56 -0300

    feat: remove useless routes

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,4 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::get('/', function () {
-    return response([
-        'message' => 'Ok'
-    ], 200);
-});
-
 Route::apiResource('/contacts', ContactController::class);

5c4310a3a90c333df9ec96f2853529150f46d4eb
Styl Bandeira <stylbandeira@gmail.com>
2026-05-15 18:21:00 -0300

    feat: create service to calculate score

---
+++ app/Domain/Services/ContactScoreCalculatorService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Services;
+
+use App\Models\Contact;
+
+class ContactScoreCalculatorService
+{
+    public function calculate(Contact $contact)
+    {
+        return $contact;
+    }
+}

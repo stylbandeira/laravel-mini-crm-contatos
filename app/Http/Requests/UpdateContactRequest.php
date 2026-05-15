a95158280dc253885a9934e504a1b17cee7b9316
Styl Bandeira <stylbandeira@gmail.com>
2026-05-15 09:44:12 -0300

    feat: define validation

---
+++ app/Http/Requests/UpdateContactRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string', 'max:255'],
+            'email' => ['sometimes', 'email', 'unique:contact,email'],
+            'phone' => ['sometimes', 'string'],
+            'score' => ['sometimes', 'prohibited'],
+            'status' => ['sometimes', 'prohibited'],
+        ];
+    }
+}
